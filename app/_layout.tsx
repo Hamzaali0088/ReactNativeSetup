b2272a8fb1cb69b7d45c2cef83f49315295cfc65
Hamzaali0088 <154756967+Hamzaali0088@users.noreply.github.com>
2025-09-18 07:56:37 -0700

    first commit

---
+++ app/_layout.tsx
@@ -0,0 +1,32 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import 'react-native-reanimated';
+import "../global.css";
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { Text } from 'react-native';
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  if (!loaded) {
+    // Async font loading only occurs in development.
+    return null;
+  }
+
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="+not-found " />
+      </Stack>
+      <StatusBar style="auto" />
+      <Text className='text-red-500 overflow-y-scroll' style={{ overflowY: 'scroll'}}>Test</Text>
+    </ThemeProvider>
+  );
+}
